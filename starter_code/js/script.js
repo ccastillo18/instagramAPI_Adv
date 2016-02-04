effe94c81a6e8864154b9266622a77b0c090d765
ccastillo18 <ccastillo@cristoreyny.org>
2016-02-04 13:49:00 +0000

    Flickr Search Project

--- starter_code/js/script.js
+++ starter_code/js/script.js
@@ -1,6 +1,26 @@
 // YOUR CODE GOES HERE
-var clientID = "REPLACE ME WITH CLIENT ID";
+var clientID = "6d65b1ee6ea2e52a8c92c681c0d2af37";
 
 $(document).ready(function() {
-    // your code here
+    $("#search-btn").click(function() {
+        var input = $("#search-query").val();
+        searchFlickr(clientID, input, 10, function(photos) {
+
+            for (var i  = 0; i < photos.photo.length; i++) {
+                $("#search-results").append("<img src=" + photos.photo[i].url_m + ">");
+
+            };
+
+
+        });
+
+
+
+    });
+
+    $("#reset-btn").click(function() {
+        $("#search-results").empty();
+    })
+
 });
+ 
